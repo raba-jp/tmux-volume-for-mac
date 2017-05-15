df2eacda2e2d9632e38f52f4fcf784f70c932691
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-05-15 11:05:26 +0900

    Initial Commit

---
+++ scripts/volume-for-mac.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+is_mac_os() {
+	[ $(uname) = 'Darwin' ] && echo 'true' || echo 'false'
+}
+
+get_volume_info() {
+	echo "$(osascript -e 'get volume settings')"
+}
+
+get_volume_value() {
+	local info=`get_volume_info`
+	local volume=$(expr $(echo "${info}" | awk '{print $2}' | sed "s/[^0-9]//g") / 6 )
+	local str=""
+	for ((i=0; i < ${volume}; i++)); do
+		str="${str}■"
+	done
+	for ((i=${volume}; i < 16; i++)); do
+		str="${str} "
+	done
+	echo "#[bold][${str}]#[default]"
+}
+
+is_muted() {
+	local info=`get_volume_info`
+	[ $(echo ${info} | awk '{print $8}') = "muted:true" ] && echo 'true' || echo 'false'
+}
+
+get_volume() {
+	local muted_string="#[bold][      mute      ]#[default] "
+	local volume_string=`get_volume_value`
+	[ `is_mac_os` = 'false' ] && return 0
+#
+	[ `is_muted` = 'true' ] && echo ${muted_string} || ${volume_string}
+}
+get_volume
---
+++ volume-for-mac.tmux
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${source_path}/scripts/helpers.sh"
+
+do_interpolation() {
+	local string="$1"
+	local interpolation_string="\#{volume_mac}"
+	local replaced_string="#(${source_path}/scripts/volume-for-mac.sh)"
+	local interpolated="${string/${interpolation_string}/${replaced_string}}"
+	printf %s "$interpolated"
+}
+
+main() {
+	update_tmux_option "status-right"
+	update_tmux_option "status-left"
+}
+
+main
+
+unset source_path
